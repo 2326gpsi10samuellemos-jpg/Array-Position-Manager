ab9576032c980dc237f2777eaf3e70f52fc59ce0
Samuel Martins Lemos <2326.gpsi.10.samuel.lemos@eptoliva.pt>
2025-10-03 17:26:26 +0100

    Add files via upload

---
+++ Array10preenchimento/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Array10preenchimento
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_num = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cb_ord = new System.Windows.Forms.ComboBox();
+            this.btn_gravar = new System.Windows.Forms.Button();
+            this.btn_mostrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(17, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(9, 0, 9, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(577, 52);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Inserção de Array";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Insira o número:";
+            // 
+            // tb_num
+            // 
+            this.tb_num.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_num.Location = new System.Drawing.Point(186, 90);
+            this.tb_num.Name = "tb_num";
+            this.tb_num.Size = new System.Drawing.Size(150, 30);
+            this.tb_num.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(171, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Insira a posição:";
+            // 
+            // cb_ord
+            // 
+            this.cb_ord.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_ord.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cb_ord.FormattingEnabled = true;
+            this.cb_ord.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.cb_ord.Location = new System.Drawing.Point(186, 134);
+            this.cb_ord.Name = "cb_ord";
+            this.cb_ord.Size = new System.Drawing.Size(150, 28);
+            this.cb_ord.TabIndex = 4;
+            // 
+            // btn_gravar
+            // 
+            this.btn_gravar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_gravar.Location = new System.Drawing.Point(356, 95);
+            this.btn_gravar.Name = "btn_gravar";
+            this.btn_gravar.Size = new System.Drawing.Size(116, 67);
+            this.btn_gravar.TabIndex = 5;
+            this.btn_gravar.Text = "Gravar";
+            this.btn_gravar.UseVisualStyleBackColor = true;
+            this.btn_gravar.Click += new System.EventHandler(this.btn_gravar_Click);
+            // 
+            // btn_mostrar
+            // 
+            this.btn_mostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_mostrar.Location = new System.Drawing.Point(478, 95);
+            this.btn_mostrar.Name = "btn_mostrar";
+            this.btn_mostrar.Size = new System.Drawing.Size(116, 67);
+            this.btn_mostrar.TabIndex = 6;
+            this.btn_mostrar.Text = "Mostrar";
+            this.btn_mostrar.UseVisualStyleBackColor = true;
+            this.btn_mostrar.Click += new System.EventHandler(this.btn_mostrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(27F, 52F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 183);
+            this.Controls.Add(this.btn_mostrar);
+            this.Controls.Add(this.btn_gravar);
+            this.Controls.Add(this.cb_ord);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tb_num);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_num;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cb_ord;
+        private System.Windows.Forms.Button btn_gravar;
+        private System.Windows.Forms.Button btn_mostrar;
+    }
+}
+
---
+++ Array10preenchimento/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Array10preenchimento
+{
+    public partial class Form1 : Form
+    {
+        int[] array = new int[10];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_gravar_Click(object sender, EventArgs e)
+        {
+            try { 
+                int a = cb_ord.SelectedIndex;
+                if (array[a] == 0)
+                    array[a] = Int32.Parse(tb_num.Text);
+                else MessageBox.Show("Array já preenchido.");
+            }
+            catch(OverflowException)
+            {
+                MessageBox.Show("Insira números menores.");
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("Insira apenas números.");
+            }
+            catch
+            {
+                MessageBox.Show("Erro Geral.");
+            }
+        
+        }
+
+        private void btn_mostrar_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                MessageBox.Show($"Posição {i+1}: {array[i]}");
+            }
+        }
+    }
+}
